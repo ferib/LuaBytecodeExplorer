bfbd214b5541e479570bc20be3133c32489df998
= <lopke123@live.nl>
2021-04-09 02:01:29 +0200

    fixed alignments

--- js/inspector.js
+++ js/inspector.js
@@ -1,9 +1,19 @@
 let inspector = function (){
 	let inspectorObjects;
 	let funcs;
+	let lines;
+	let blocks;
+	let currentFunc;
+	let currentScript;
+	let linesCount;
 	
 	let _init = function() {
 		funcs = [];
+		blocks = [];
+		lines = [];
+		linesCount = [];
+		blocksCount = [];
+		funcsCount = [];
 	}
 	
 	let _initFromUrl = function(callback) {
@@ -19,23 +29,65 @@ let inspector = function (){
 	}
 	
 	let _parseScript = function(func) {
+		if(func == currentFunc)
+			return currentScript;
+		
 		console.log("[Writing] " + func.ScriptFunction.Name)
-		var script = $("<span>"); 
+		currentScript = $("<span>"); 
 		$.each(func.ScriptFunction.Blocks, function(i, block) {
 			var blockText = "";
 			$.each(block.Lines, function(i, line) {
 				blockText += line.Text;
 			})
-			script.append(
-				$("<span>").addClass("sblock-if").text(blockText) // questionable
+			
+			var addNoteClass = "";
+			
+			// rules based on https://ferib.dev/blog.php?l=post/Lua_Devirtualization_Part_2_Decompiling_Lua#BlockDefinitions
+			if(block.JumpsTo != -1 && block.JumpsNext != -1) // TODO: check opcode?
+				addNoteClass = "sblock-if";
+			else if (block.JumpsTo != -1 && block.JumpsNext == -1)
+				addNoteClass = "sblock-else";
+			else if (block.JumpsTo == -1 && block.JumpsNext != -1)
+				addNoteClass = "sblock-endif";
+			else if (block.JumpsTo == -1 && block.JumpsNext == -1)
+				addNoteClass = "sblock-end";
+			
+			currentScript.append(
+				$("<span>").addClass(addNoteClass).text(blockText) // questionable
 			)
 		})
-		return script;
+		return currentScript;
+	}
+	
+	let _getLines = function() {
+		lines = $("<ul>");
+		// TODO
+	}
+	
+	let _highlightLine = function(funcIndex, lineIndex) {
+		var offset = 0;
+		for(var i = 0; i < funcIndex; i++)
+		{
+			offset += linesCount[i];
+		}
+		return lineIndex - offset;
+	}
+	
+	let _highlightBlock = function(funcIndex, blockIndex) {
+		var offset = 0;
+		for(var i = 0; i < funcIndex; i++)
+		{
+			offset += blocksCount[i]
+		}	
+		return blockIndex - offset;
 	}
 	
 	// TODO: this is ugly, cleanup?
 	let _parseFunc = function(index, func) {
 		index++;
+		var lcount = 0;
+		var bcount = 0;
+		funcsCount++;
 		console.log("[Parsing][" + func.ScriptFunction.Name + "] Blocks count: " + func.ScriptFunction.Blocks.length);
 		funcs.push(func) // save reference for later
 		
@@ -53,37 +105,40 @@ let inspector = function (){
 			$("<li>").addClass("ins-list").append($("<a>").attr("href", "#").text("[Constants] (" + func.Constants.length + ")")).append($("<span>").append(constantsInspector)),
 			$("<li>").addClass("ins-list").append($("<a>").attr("href", "#").text("[Blocks] (" + func.ScriptFunction.Blocks.length + ")")).append($("<span>").append(blockInspector)),
 			$("<li>").addClass("ins-list").append($("<a>").attr("href", "#").text("[Functions] (" + func.Functions.length + ")")).append($("<span>").append(childFuncInspector))
-		);
+		).addClass("insp-func");
 		
 		// build Blocks (array)		
-		$.each(func.ScriptFunction.Blocks, function(i, item) {
+		$.each(func.ScriptFunction.Blocks, function(i, block) {
 			var newBlock = $("<ul>").addClass("inspector").addClass("hide");
-			newBlock.append($("<li>").addClass("ins-item").text("ifChainIndex").append($("<span>").text(item.ifChainIndex)));
-			newBlock.append($("<li>").addClass("ins-item").text("JumpsNext").append($("<span>").text(item.JumpsTo)));
-			newBlock.append($("<li>").addClass("ins-item").text("JumpsTo").append($("<span>").text(item.JumpsNext)));
+			newBlock.append($("<li>").addClass("ins-item").text("ifChainIndex").append($("<span>").text(block.ifChainIndex)));
+			newBlock.append($("<li>").addClass("ins-item").text("JumpsNext").append($("<span>").text(block.JumpsTo)));
+			newBlock.append($("<li>").addClass("ins-item").text("JumpsTo").append($("<span>").text(block.JumpsNext)));
 			
 			// append Line to Lines
 			// append Instruction to Instructions
 			var blockLines = $("<ul>").addClass("inspector").addClass("hide");
 			var blockInstructions = $("<ul>").addClass("inspector").addClass("hide");
-			$.each(item.Lines, function(i, b) {
+			$.each(block.Lines, function(i, b) {
 				blockLines.append(
-					$("<li>").addClass("ins-item").append(
-						$("<code>").text("[" + i + "]: " + b.Text).addClass("insp-code")
+					$("<li>").addClass("ins-item").addClass("insp-line").append(
+						$("<code>").text("[" + (block.StartAddress + i) + "]: " + b.Text)
 					)
 				)
-				var instr = item.Lines[i].Instr;
+				var instr = block.Lines[i].Instr;
 				blockInstructions.append(
-					$("<li>").addClass("ins-item").append(
-						$("<span>").text("[" + i + "]")
-					).text(instr.Text) // TODO fix this
-				);
+					$("<li>").addClass("ins-item").addClass("insp-opcode").append(
+						$("<code>").text("[" + (block.StartAddress + i) + "] " + instr.Text) // TODO fix this
+					)
+				)
+				
+				lcount++;
+				lines.push(b);
 			})
 			
 			// append Lines to Block
 			newBlock.append(
 				$("<li>").addClass("ins-list").append(
-					$("<a>").attr("href", "#").text("[Lines] (" + item.Lines.length + ")")
+					$("<a>").attr("href", "#").text("[Lines] (" + block.Lines.length + ")")
 				).append(
 					$("<span>").append(blockLines)
 				)
@@ -92,7 +147,7 @@ let inspector = function (){
 			// append Instructions to Block
 			newBlock.append(
 				$("<li>").addClass("ins-list").append(
-					$("<a>").attr("href", "#").text("[Instructions] (" + item.Lines.length + ")")
+					$("<a>").attr("href", "#").text("[Instructions] (" + block.Lines.length + ")")
 				).append(
 					$("<span>").append(blockInstructions)
 				)
@@ -101,21 +156,24 @@ let inspector = function (){
 			var addNoteClass = "";
 			
 			// rules based on https://ferib.dev/blog.php?l=post/Lua_Devirtualization_Part_2_Decompiling_Lua#BlockDefinitions
-			if(item.JumpsTo != -1 && item.JumpsNext != -1) // TODO: check opcode?
+			if(block.JumpsTo != -1 && block.JumpsNext != -1) // TODO: check opcode?
 				addNoteClass = "block-if";
-			else if (item.JumpsTo != -1 && item.JumpsNext == -1)
+			else if (block.JumpsTo != -1 && block.JumpsNext == -1)
 				addNoteClass = "block-else";
-			else if (item.JumpsTo == -1 && item.JumpsNext != -1)
+			else if (block.JumpsTo == -1 && block.JumpsNext != -1)
 				addNoteClass = "block-endif";
-			else if (item.JumpsTo == -1 && item.JumpsNext == -1)
+			else if (block.JumpsTo == -1 && block.JumpsNext == -1)
 				addNoteClass = "block-end";
 				
 			// append Block to Function
 			blockInspector.append(
-				$("<li>").addClass("ins-list").append($("<a>").attr("href", "#").text("[" + i + "]").append($("<span>").addClass("lasm-note").text(" - " + item.StartAddress + ": JMP: " + item.JumpsTo + ", ELSE: " + item.JumpsNext))).addClass(addNoteClass).append(
+				$("<li>").addClass("ins-list").append($("<a>").addClass("insp-block").attr("href", "#").text("[" + i + "]").append($("<span>").addClass("lasm-note").text(" - " + block.StartAddress + ": JMP: " + block.JumpsTo + ", ELSE: " + block.JumpsNext))).addClass(addNoteClass).append(
 					$("<span>").append(newBlock)
 				)
 			)
+			
+			bcount++;
+			blocks.push(block);
 		});
 		
 		// build Upvalues
@@ -132,6 +190,9 @@ let inspector = function (){
 				$("<li>").addClass("ins-item").text("[" + i + "]").append($("<span>").text("\"" + item.Value + "\""))
 			)
 		})
+
+		linesCount.push(lcount);
+		blocksCount.push(bcount);
 
 		// append Functions to Function
 		$.each(func.Functions, function(i, item) {
@@ -151,7 +212,11 @@ let inspector = function (){
 		parseFunc: _parseFunc,
 		buildInspector: _buildInspector,
 		writeFunc: _writeFunc,
-		initFromUrl: _initFromUrl
+		initFromUrl: _initFromUrl,
+		getLines: _getLines,
+		highlightLine: _highlightLine,
+		highlightBlock: _highlightBlock
 	}
 }();
+
 inspector.init();
