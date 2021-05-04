5183cbc53feb93653b69faa33b7814aa58d9db60
= <lopke123@live.nl>
2021-05-04 18:05:07 +0200

    add ui

---
+++ js/ui.js
@@ -0,0 +1,23 @@
+let ui = function() {
+
+    let _init = function() {
+
+    }
+
+    let _showLoader = function() {
+        // build loader
+        var background = $("<div>").addClass("splashscreen")
+        background.append($("<div>").addClass)
+        return background;
+    }
+
+    let _hideLoader = function() {
+
+    }
+
+    return {
+        init: _init,
+        showLoader: _showLoader,
+        hideLoader: _hideLoader
+    }
+}();
